f413994b7c757a7f9827ab3fe91e324165e3aaef
RomainC <romain.caplier@student-cs.fr>
2022-05-11 14:42:30 +0200

    Updated the download script

--- download.py
+++ download.py
@@ -1,125 +1,157 @@
-from bs4 import BeautifulSoup
-import requests
 import os
+import glob
+import time
+
+import requests
+from bs4 import BeautifulSoup
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 
 from tqdm.auto import tqdm
 
-from utilities import protected_get
-from selenium_class import SeleniumContainer
 
+class Downloader():
 
-def get_pdf_pubmed(url, selenium_cont):
-    """
-    Given a pubmed url, search the page to find the pdf of the article (through external sites as the actual pdf is not hosted on pubmed)
-    """
+    def __init__(self):
 
-    # Setting up the name of the pdf
-    name = url.split('/')[-1]
-    if name == "":
-        name = url.split('/')[-2]
+        self.download_dir = "/home/romainc/code/Anastomosis/data/"
 
-    if name in [a.split('.')[0] for a in os.listdir('data')]:
-        return
+        # Options and Service
+        op = Options()
+        ser = Service(ChromeDriverManager().install())
 
-    # Getting the inital page
-    page = protected_get(url)
+        op.add_experimental_option('prefs', {"download.default_directory": self.download_dir,
+                                             "plugins.always_open_pdf_externally": True})
 
-    # Making the soup, and finding links to the mirrors
-    soup = BeautifulSoup(page.text, features="lxml")
-    try:
-        dl_link = soup.find("div", {"class": "full-text-links"}).find("a", {"class": "link-item"})
-        dl_url = dl_link.get('href')
-        dl_page_type = dl_link.get('data-ga-action')
-    except:
-        print(str(name) + " - No pdf download links")
-        return
+        # send browser option to webdriver object
+        self.driver = webdriver.Chrome(service=ser, options=op)
 
-    # Making the request of the actual download page
-    with protected_get(dl_url) as dl_page:
+    def _pget(self, url, stream=False):
+        """
+        Acounts for network errors in getting a request (Pubmed often appears offline, but not for long periods of time)
+        Retries every 2 seconds for 60 seconds and then gives up
+        """
+        downloaded = False
+        count = 0
 
-        sele_state = False
+        while not downloaded and count < 30:
+            try:
+                page = requests.get(url, stream=stream)
+                downloaded = True
+            except:
+                print(url + f" - Network error, retrying... ({count + 1})")
+                time.sleep(2)
+                count += 1
 
-        # Making the soup
-        dl_soup = BeautifulSoup(dl_page.text, features="lxml")
+        if page != None:
+            return page
+        else:
+            raise ValueError
 
-        # Getting the pdf link from all the different sources
-        if dl_page_type == "Springer":
-            pdf_link = dl_soup.find("div", {"class": "c-pdf-download"}).find("a", {"class": "u-button"})
-            pdf_url = pdf_link.get("href")
-        elif dl_page_type == "Wiley":
+    def _get_search_matches(self, search_terms):
+        """
+        From a collection of search terms, get all of the ids of articles matching those search criterias
+        """
+        # Filtering out every non English match
+        search_url = "https://pubmed.ncbi.nlm.nih.gov/?term=" + '+'.join(search_terms) + '&filter=lang.english'
+        full_search_ids = []
+
+        # Grabs every page
+        page_num = 0
+        while page_num < 2:
+            page_num += 1
+            if page_num != 1:
+                page_url = search_url + "&page=" + str(page_num)
+            else:
+                page_url = search_url
+            try:
+                page = self._pget(page_url)
+                page_soup = BeautifulSoup(page.text, features="lxml")
+                page_ids = page_soup.find("div", {"class": "search-results-chunk results-chunk"}
+                                          ).get("data-chunk-ids").split(",")
+                full_search_ids += page_ids
+            except AttributeError:
+                break
+
+        # Saving the results
+        self.full_search_ids = full_search_ids
+
+    def _get_pdf_url(self, pdf_id):
+        """
+        Given a Pubmed article id, finds the url of the corresponding pdf, if freely available
+        Needs manual implementation for each publishing site, feeel free to add some
+        """
+        # Tracks whether or not the url was found
+        found = True
+
+        # Getting the inital page
+        pubmed_url = "https://pubmed.ncbi.nlm.nih.gov/" + pdf_id + "/"
+        pubmed_page = self._pget(pubmed_url)
+        pubmed_soup = BeautifulSoup(pubmed_page.text, features="lxml")
+
+        # Grabbing download links if available
+        try:
+            dl_features = pubmed_soup.find("div", {"class": "full-text-links"}).find("a", {"class": "link-item"})
+            dl_url = dl_features.get('href')
+            dl_page_type = dl_features.get('data-ga-action')
+        except:
+            return False, str(pdf_id) + " - No pdf download links"
+
+        if dl_page_type == "Wiley":
             pdf_url = "https://onlinelibrary.wiley.com/doi/pdfdirect/" + \
                 '/'.join(dl_url.split("/")[-2::]) + "?download=true"
-            selenium_cont.get_pdf(pdf_url, name)
-            sele_state = True
-            print(str(name) + " - Downloaded")
+
+        elif dl_page_type == "Springer":
+            with self._pget(dl_url) as page_dl:
+                soup_dl = BeautifulSoup(page_dl.text, features="lxml")
+                pdf_url = soup_dl.find("div", {"class": "c-pdf-download"}).find("a", {"class": "u-button"}).get('href')
+
         else:
-            print(str(name) + " - Type not set up: ", dl_page_type)
-            return dl_page_type
+            found = False
+            pdf_url = str(pdf_id) + " - Type not set up: " + dl_page_type
 
-        # Downloading the pdf
-        if not sele_state:
-            try:
-                pdf = protected_get(pdf_url, stream=True)
-                with tqdm.wrapattr(open(os.path.join("data", name + '.pdf'), "wb"), "write", miniters=1, total=int(pdf.headers.get('content-length', 0)), desc=name) as f_out:
-                    for chunk in pdf.iter_content(chunk_size=4096):
-                        f_out.write(chunk)
-                print(str(name) + " - Downloaded")
-            except:
-                print(str(name) + " - Not downloaded")
+        return found, pdf_url
 
+    def _download_pdf(self, pdf_id, pdf_url):
+        """
+        Given the url of a pdf, downloads it (should bypass DDOS protection mecanisms)
+        """
+        self.driver.get(pdf_url)
+        time.sleep(0.2)
+        latest_file = max(glob.glob(self.download_dir + "*"), key=os.path.getctime)
+        os.rename(latest_file, self.download_dir + pdf_id + '.pdf')
 
-def get_results_pubmed(search_terms):
+    def download(self, search_terms):
+        """
+        Downloads the pdfs of matching search results
+        """
+        print("\n\nGrabbing all search results...")
+        self._get_search_matches(search_terms)
+        print(f"Found {len(self.full_search_ids)} matching documents.")
 
-    sele = SeleniumContainer()
-
-    url_search = "https://pubmed.ncbi.nlm.nih.gov/?term=" + '+'.join(search_terms) + '&filter=lang.english'
-    search_results_ids = []
-
-    print("Grabbing all search results...")
-    downloaded = False
-
-    page = protected_get(url_search)
-    soup = BeautifulSoup(page.text, features="lxml")
-    page_ids = soup.find("div", {"class": "search-results-chunk results-chunk"}).get("data-chunk-ids").split(",")
-
-    search_results_ids += page_ids
-
-    page_num = 1
-
-    while page_num < 50:
-        page_num += 1
-        url_page = url_search + "&page=" + str(page_num)
-        try:
-            page = protected_get(url_page)
-            soup = BeautifulSoup(page.text, features="lxml")
-            page_ids = soup.find("div", {"class": "search-results-chunk results-chunk"}
-                                 ).get("data-chunk-ids").split(",")
-            search_results_ids += page_ids
-        except AttributeError:
-            break
-
-    print("Documents to download: ", len(search_results_ids))
-    undefined_page_types = {}
-    print("Downloading...")
-
-    for name in search_results_ids:
-        url = "https://pubmed.ncbi.nlm.nih.gov/" + name + "/"
-        page_type = get_pdf_pubmed(url, sele)
-        if isinstance(page_type, str):
-            if page_type in undefined_page_types.keys():
-                undefined_page_types[page_type] += 1
+        print("Downloading...")
+        log = "Logged actions -------------------\n"
+        for pdf_id in tqdm(self.full_search_ids[:5]):
+            found, pdf_url = self._get_pdf_url(pdf_id)
+            if found:
+                self._download_pdf(pdf_id, pdf_url)
+                log += pdf_id + " - Downloaded\n"
             else:
-                undefined_page_types[page_type] = 1
+                log += pdf_url + "\n"
 
-    unknown_types = ""
-    for key in undefined_page_types:
-        unknown_types += key + ": " + str(undefined_page_types[key]) + "\n"
-    with open(os.path.join("data", "types_missing.txt"), "w") as f:
-        f.write(unknown_types)
+        print(log)
+        with open("download_log.txt", "w") as f:
+            f.write(log)
+
+        self.driver.close()
 
 
-if not os.path.exists("data"):
-    os.makedirs("data")
+if __name__ == "__main__":
+    if not os.path.exists("data"):
+        os.mkdir("data")
 
-search = ["anastomotic", "leakage"]
-get_results_pubmed(search)
+    dl = Downloader()
+    dl.download(['anastomotic', 'leak'])
